74fb4f782dee43c90e710a49e4f6d91a80c47751
Michael Lyon <mlyon360@gmail.com>
2024-04-04 17:04:37 -0600

    format with black

--- 01.py
+++ 01.py
@@ -1,15 +1,15 @@
-with open('input.txt') as f:
-  chunks = f.read().split('\n\n')
-  calorieCounts = [sum(tuple(map(int, chunk.split()))) for chunk in chunks]
+with open("input.txt") as f:
+    chunks = f.read().split("\n\n")
+    calorieCounts = [sum(tuple(map(int, chunk.split()))) for chunk in chunks]
 
-  topThree = (0, 0, 0)
+    topThree = (0, 0, 0)
 
-  for count in calorieCounts:
-    if (count > topThree[2]):
-      topThree = topThree[1], topThree[2], count
-    elif (count > topThree[1]):
-      topThree = topThree[1], count, topThree[2]
-    elif (count > topThree[0]):
-      topThree = count, topThree[1], topThree[2]
-  
-  print(sum(topThree))
+    for count in calorieCounts:
+        if count > topThree[2]:
+            topThree = topThree[1], topThree[2], count
+        elif count > topThree[1]:
+            topThree = topThree[1], count, topThree[2]
+        elif count > topThree[0]:
+            topThree = count, topThree[1], topThree[2]
+
+    print(sum(topThree))
