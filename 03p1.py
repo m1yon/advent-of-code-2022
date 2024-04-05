22436a2dc1265c0e265881fc811b89c22d7c1c3d
Michael Lyon <mlyon360@gmail.com>
2024-04-05 16:53:53 -0600

    cleanup

--- 03p1.py
+++ 03p1.py
@@ -1,9 +1,5 @@
-def get_priority(item):
-    code_point = ord(item)
-    if code_point >= ord("a") and code_point <= ord("z"):
-        return code_point - 96
-
-    return code_point - 64 + 26
+def get_priority(item_type):
+    return ord(item_type) - (96 if item_type >= "a" else 38)
 
 
 with open("input.txt") as f:
@@ -11,7 +7,7 @@ with open("input.txt") as f:
 
     sum = 0
     for item in rucksacks:
-        split_index = int(len(item) / 2)
+        split_index = len(item) // 2
         first_set, second_set = (set(item[:split_index]), set(item[split_index:]))
         duplicate = first_set.intersection(second_set).pop()
         priority = get_priority(duplicate)
